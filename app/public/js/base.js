9b370bc55892566ec92881ad347c57622d1a1eef
Brian Prost <bprost94@gmail.com>
2015-01-25 04:54:57 +0000

    added ui-router dependency

--- app/public/js/base.js
+++ app/public/js/base.js
@@ -3,9 +3,14 @@
  * include it in your layout.jade file. It's pretty easy either way.
  **/
 
-var app = angular.module("BlocItOff", ['firebase']);
+var app = angular.module("BlocItOff", [
+    'firebase',
+    'ui-router'
+    ]);
 
 app.controller("FRBcontroller", function($scope, $firebase) {
     var ref = new Firebase("https://bloc-it-off.firebaseio.com/");
     var sync = $firebase(ref);
+    ref.set({name: 'Brian Prost'});
 })
+
